b83c05511f63dbe6358e0be5fc84641594a64f9f
James <gekylin@qq.com>
2020-05-11 10:14:46 +0800

    update

---
+++ spring-boot-hello-world/src/main/java/com/james/example/boot/hello/world/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.james.example.boot.hello.world.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author James
+ * @date 2020/5/11
+ */
+@RestController
+public class UserController {
+
+
+}
