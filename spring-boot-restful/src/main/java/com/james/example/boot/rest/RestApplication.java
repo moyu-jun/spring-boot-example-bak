5c712519c6ef769e215941613f8ee1ae04cdf146
James <gekylin@qq.com>
2020-05-11 10:25:43 +0800

    update

---
+++ spring-boot-restful/src/main/java/com/james/example/boot/rest/RestApplication.java
@@ -0,0 +1,17 @@
+package com.james.example.boot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author James
+ * @date 2020/5/9
+ */
+@SpringBootApplication
+public class RestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+}
---
+++ spring-boot-swagger/src/main/java/com/james/example/boot/swagger/SwaggerApplication.java
@@ -0,0 +1,17 @@
+package com.james.example.boot.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author James
+ * @date 2020/5/9
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+}
